699839c6607235f6106a9ad88744df0365b492eb
YeonjinJo <hrj8441@naver.com>
2024-01-26 17:01:46 +0900

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 function App() {
-  const [students, setStudents] = useState([
+  const students = [
     { name: "Alice", age: 17, grade: "A" },
     { name: "Bob", age: 18, grade: "B" },
     { name: "Charlie", age: 16, grade: "C" },
@@ -14,28 +14,38 @@ function App() {
     { name: "Jenny", age: 25, grade: "D" },
     { name: "Kevin", age: 26, grade: "E" },
     { name: "Linda", age: 27, grade: "F" },
-  ]);
+  ];
   const [filteredStudents, setFilteredStudents] = useState(students);
+  const [minAge, setMinAge] = useState(0);
 
   // TODO: filterByAge 함수를 작성하세요. 이 함수는 최소 나이를 매개변수로 받아 해당 나이 이상인 학생들로 필터링해야 합니다.
   const filterByAge = (minAge) => {
-    // 여기에 코드를 작성하세요.
+    const filteredList = students.filter((element) => element.age >= minAge);
+    setFilteredStudents(filteredList);
   };
 
   // TODO: filterByGrade 함수를 작성하세요. 이 함수는 특정 학점을 매개변수로 받아 해당 학점의 학생들로 필터링해야 합니다.
   const filterByGrade = (grade) => {
-    // 여기에 코드를 작성하세요.
+    const filteredList = students.filter((element) => element.grade === grade);
+    setFilteredStudents(filteredList);
   };
 
   // TODO: resetFilter 함수를 작성하세요. 이 함수는 필터를 초기화하여 모든 학생들을 표시해야 합니다.
   const resetFilter = () => {
-    // 여기에 코드를 작성하세요.
+    setFilteredStudents(students);
   };
 
   return (
     <div>
       <h1>학생 목록</h1>
-      <button onClick={() => filterByAge(24)}>24세 이상</button>
+      <input
+        value={minAge}
+        onChange={(event) => {
+          setMinAge(event.target.value);
+        }}
+        type="number"
+      />
+      <button onClick={() => filterByAge(minAge)}>세 이상</button>
       <button onClick={() => filterByGrade("A")}>A등급</button>
       <button onClick={resetFilter}>필터 초기화</button>
       <ul>
