1024383fc93092d3a6a986ac8347d00a94385571
YeonjinJo <hrj8441@naver.com>
2024-01-26 16:28:08 +0900

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,18 @@ function App() {
 
   // TODO: filter를 사용하여 18세 이상의 학생들만 선택하세요.
   const filteredStudents = students.filter((element) => element.age >= 18 ? true : false);
-  const mapped = students.map(())
 
   return (
     <div>
       <h1>학생 목록</h1>
       <ul>
-        {/* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */}
+        filteredStudents.map((element, index) => {
+          return (
+            <ul onClick={() => {
+              alert(`나이는 ${student.age}이고, 등급은 ${student.grade}입니다.`)
+            }} key={index}></ul>
+          )
+        })
         {/* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/}
       </ul>
     </div>
